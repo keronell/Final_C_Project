82534f11df1fa9ab8b7a0367053378b56d41c278
keronell <thevlamax@gmail.com>
2024-04-11 22:16:30 +0300

    Space control almost done To do: - sort functions - space map

--- SpaceControlSystem.c
+++ SpaceControlSystem.c
@@ -1,5 +1,75 @@
-//
-// Created by Vlad Pavlyuk on 03/04/2024.
-//
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
 #include "SpaceControlSystem.h"
+
+
+int initSystem(SpaceControlSystem* pSystem)
+{
+    if (pSystem == NULL)
+        return -1; // Indicate error
+
+
+    // Allocate memory for the array of CelestialBody pointers with initial size of 1
+    pSystem->CelestialBodyArr = (CelestialBody**)malloc(sizeof(CelestialBody*));
+    if (pSystem->CelestialBodyArr == NULL)
+        return -1; // Indicate error
+
+    pSystem->CelestialBodyArr[0] = NULL; // Initialize the first pointer to NULL
+
+    // Set numOfBodies to 0
+    pSystem->numOfBodies = 0;
+
+    // Set a default sorting option
+    pSystem->sortOpt = eID; // Or any other default sorting option
+
+    return 0; // Success
+}
+
+int addCelestialBody(SpaceControlSystem* pSystem, CelestialBody* newBody)
+{
+    if (pSystem == NULL || newBody == NULL)
+    {
+        printf("Error: Invalid parameters.\n");
+        return -1;
+    }
+
+    // Resize the array to hold one more CelestialBody pointer
+    CelestialBody** tempArr = (CelestialBody**)realloc(pSystem->CelestialBodyArr, (pSystem->numOfBodies + 1) * sizeof(CelestialBody*));
+    if (tempArr == NULL)
+    {
+        printf("Error: Memory reallocation failed.\n");
+        return -1;
+    }
+
+    pSystem->CelestialBodyArr = tempArr;
+    pSystem->CelestialBodyArr[pSystem->numOfBodies] = newBody;
+    pSystem->numOfBodies++;
+
+    return 0; // Success
+}
+
+void printSpaceControlSystem(const SpaceControlSystem* pSystem)
+{
+    if (pSystem == NULL)
+    {
+        printf("Error: SpaceControlSystem pointer is NULL.\n");
+        return;
+    }
+
+    printf("Number of Celestial Bodies: %d\n", pSystem->numOfBodies);
+    printf("Celestial Bodies:\n");
+
+    for (int i = 0; i < pSystem->numOfBodies; i++)
+    {
+        if (pSystem->CelestialBodyArr[i] != NULL)
+        {
+            printCelestialBody(pSystem->CelestialBodyArr[i]);
+        }
+        else
+        {
+            printf("Celestial Body %d: NULL\n", i);
+        }
+    }
+}
--- SpaceControlSystem.h
+++ SpaceControlSystem.h
@@ -1,21 +1,25 @@
 #ifndef FINAL_C_PROJECT_SPACECONTROLSYSTEM_H
 #define FINAL_C_PROJECT_SPACECONTROLSYSTEM_H
 #include "GeneralList.h"
+#include "CelestialBody.h"
 
 typedef enum { eID, eSize, eDistance,eFoundBy,eType, eNofSortOpt } eSortOption;
 static const char* sortOptStr[eNofSortOpt];
 
 
-typedef struct SpaceControlSystem
+typedef struct
 {
-    LIST    CelestialBodyList;
+    CelestialBody**    CelestialBodyArr;
     int     numOfBodies;
     eSortOption sortOpt;
 
-};
+}SpaceControlSystem;
 
-int     initSystemFromFile();
-void    initSystem();
+int        initSystem(SpaceControlSystem* pSystem);
+
+int addCelestialBody(SpaceControlSystem* pSystem, CelestialBody* newBody);
 
 void    spaceMap();
+
+void printSpaceControlSystem(const SpaceControlSystem* pSystem);
 #endif //FINAL_C_PROJECT_SPACECONTROLSYSTEM_H
